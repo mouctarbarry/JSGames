6a8ba8f7fbe309f6397cc479ec9b0df1c0bfa7e9
mouctabarry <bmouctar22@gmail.com>
2023-05-20 19:47:24 +0200

    Creating the snake

--- script.js
+++ script.js
@@ -1,12 +1,86 @@
-console.log("Hello Mouctar");
+window.onload = function (){
 
-let speed = 100;
-if (speed <50){
-    console.log("Tu roules pas assez vite")
+    let canvas;
+    let ctx;
+    let delay = 100;
+    let canvasWidth = 900;
+    let canvasHeight = 600;
+    let snakee;
+    let blockSize = 30;
+
+
+    init();
+   function init(){canvas = document.createElement('canvas');
+       canvas.width = canvasWidth;
+       canvas.height = canvasHeight;
+       canvas.style.border = "1px solid";
+       document.body.appendChild(canvas);
+       ctx = canvas.getContext('2d');
+       snakee = new Snake([[6,4], [5,4], [4,4]]);
+       refreshCanvas();
+
+   }
+
+   function refreshCanvas(){
+       ctx.clearRect(0,0, canvas.width, canvas.height);
+       snakee.draw();
+       setTimeout(refreshCanvas, delay);
+   }
+
+   function drawBlock(ctx, position){
+       let x= position[0] * blockSize;
+       let y= position[1] * blockSize;
+       ctx.fillRect(x, y, blockSize, blockSize);
+   }
+   function Snake(body){
+       this.body = body;
+       this.draw = function(){
+           ctx.save();
+           ctx.fillStyle = "#ff0000";
+           for (let i = 0; this.body.length; i++){
+               drawBlock(ctx, this.body[i]);
+           }
+           ctx.restore();
+       };
+   }
 }
 
-function affiche(){
-    console.log("Bonjour tout le monde");
-    return true;
-}
-console.log(affiche());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
