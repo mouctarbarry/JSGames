cfeb13a66b00156a0f90111e8ffd2f3b87cc05ed
mouctarbarry <mouctar.barry@sogeti.com>
2023-05-20 18:16:26 +0200

    Adding dictionary

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 console.log("Hello Mouctar");
+
+let speed = 100;
+if (speed <50){
+    console.log("Tu roules pas assez vite")
+}
+
+function affiche(){
+    console.log("Bonjour tout le monde");
+    return true;
+}
+console.log(affiche());
